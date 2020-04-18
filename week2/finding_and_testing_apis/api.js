4aedeeab5d2006a973431cd69a3a68e01a317cbd
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2020-04-18 13:36:48 -0500

    api data resubmission

--- week2/finding_and_testing_apis/api.js
+++ week2/finding_and_testing_apis/api.js
@@ -1,14 +1,36 @@
-let catFacts = {
-            "fact": "The first cat in space was a French cat named Felicette (a.k.a. “Astrocat”) In 1963, France blasted the cat into outer space. Electrodes implanted in her brains sent neurological signals back to Earth. She survived the trip.",
-            "length": 224
-        };
+let swapiData = {
+    "name": "R2-D2",
+    "height": "96",
+    "mass": "32",
+    "hair_color": "n/a",
+    "skin_color": "white, blue",
+    "eye_color": "red",
+    "birth_year": "33BBY",
+    "gender": "n/a",
+    "homeworld": "http://swapi.dev/api/planets/8/",
+    "films": [
+        "http://swapi.dev/api/films/1/",
+        "http://swapi.dev/api/films/2/",
+        "http://swapi.dev/api/films/3/",
+        "http://swapi.dev/api/films/4/",
+        "http://swapi.dev/api/films/5/",
+        "http://swapi.dev/api/films/6/"
+    ],
+    "species": [
+        "http://swapi.dev/api/species/2/"
+    ],
+    "vehicles": [],
+    "starships": [],
+    "created": "2014-12-10T15:11:50.376000Z",
+    "edited": "2014-12-20T21:17:50.311000Z",
+    "url": "http://swapi.dev/api/people/3/"
+};
 
-const value = Object.values(catFacts);
-const entries = Object.entries(catFacts);
+const values = Object.values(swapiData);
+const entries = Object.entries(swapiData);
 
-let fact = document.createElement("p");
-fact.innerHTML = catFacts.fact;
-fact.style.fontSize = "20px";
-fact.style.textAlign = "center";
-document.getElementById("apiData").appendChild(fact);
+const dataSet = document.getElementById('dataSet');
 
+dataSet.innerHTML = '<ul>' + entries.map(function (item) {  
+    return `<li><span>keys:</span> ${item[0]} <span>values:</span> ${item[1]} </li>`;         
+}).join('') + '</ul>'
